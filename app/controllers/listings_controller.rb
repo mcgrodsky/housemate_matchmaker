8803ed6a96f8ec4bb7e667675a21ed7c3ec035cd
MiaKoda Grodsky <mcgrodsky@gmail.com>
2015-11-16 18:13:24 -0500

    working routes and controllers for neighborhoods and listings

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,6 +1,58 @@
 class ListingsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
-  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+  before_action :authenticate_user!, only: [:show, :new, :create, :edit, :update, :destroy]
+  def index
+    # authenticate_user!
+    # if current_user
+    #   @listings = current_user.listings
+    # else
+      @listings = Listing.all
+    # end
+  end
+
+  def new
+      @neighborhood = Neighborhood.find(params[:neighborhood_id])
+      @listing = Listing.new
+  end
+
+  def create
+    @neighborhood = Neighborhood.find(params[:neighborhood_id])
+    @listing = @neighborhood.listings.create!(listing_params.merge(user: current_user))
+    redirect_to neighborhood_path(@neighborhood), notice: "Your listing was successfully created!!"
+  end
+
+  def show
+    @neighborhood = Neighborhood.find(params[:neighborhood_id])
+    # @listing = Listing.find(params[:id])
+  end
+
+  def edit
+    @neighborhood = Neighborhood.find(params[:neighborhood_id])
+    # @listing = Listing.find(params[:id])
+  end
+
+  def update
+    # @listing = Listing.find(params[:id])
+    @neighborhood = Neighborhood.find(params[:neighborhood_id])
+    @listing.update(listing_params.merge(user: current_user))
+    redirect_to neighborhood_listing_path(@neighborhood, @listing)
+  end
+
+  def destroy
+    # @listing = Listing.find(params[:id])
+    @neighborhood = Neighborhood.find(params[:neighborhood_id])
+    @listing.destroy
+    redirect_to neighborhood_path(@neighborhood), notice: "Your listing was successfully deleted!"
+  end
+
+  private
+  def listing_params
+    params.require(:listing).permit(:name, :age, :gender, :description)
+  end
+
+  def set_post
+    @listing = Listing.find(params[:id])
+  end
 
 
 end
--- app/controllers/neighborhoods_controller.rb
+++ app/controllers/neighborhoods_controller.rb
@@ -3,8 +3,20 @@ class NeighborhoodsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
 
 def index
+  @neighborhoods = Neighborhood.all
+end
 
+def show
+    #  @neighborhood = Neighborhood.find(params[:id])
 end
 
 
+private
+def neighborhood_params
+  params.require(:neighborhood).permit(:title, :description)
+end
+
+def set_post
+  @neighborhood = Neighborhood.find(params[:id])
+end
 end
