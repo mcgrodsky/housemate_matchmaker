7afebda44e2704c0e30a2724784d6697860a5fc9
MiaKoda Grodsky <mcgrodsky@gmail.com>
2015-11-19 08:17:32 -0500

    login working. everything working

--- app/controllers/neighborhoods_controller.rb
+++ app/controllers/neighborhoods_controller.rb
@@ -8,6 +8,7 @@ end
 
 def show
     #  @neighborhood = Neighborhood.find(params[:id])
+
 end
 
 def edit
--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,4 +1,12 @@
 class Listing < ActiveRecord::Base
   belongs_to :neighborhood
   belongs_to :user
+
+  def self.search(search)
+  if search
+    find(:all, :conditions => ['name LIKE ?', "%#{search}%"])
+  else
+    find(:all)
+  end
+end
 end
