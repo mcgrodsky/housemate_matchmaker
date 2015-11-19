87cdc5289b275ed373de15cc7f71b36245d0bd0b
MiaKoda Grodsky <mcgrodsky@gmail.com>
2015-11-19 09:04:35 -0500

    downloaded devise views to convert to bootstrap

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,12 +1,11 @@
 class Listing < ActiveRecord::Base
   belongs_to :neighborhood
   belongs_to :user
-
   def self.search(search)
-  if search
-    find(:all, :conditions => ['name LIKE ?', "%#{search}%"])
-  else
-    find(:all)
+    if search
+      find(:all, :conditions => ['name LIKE ?', "%#{search}%"])
+    else
+      find(:all)
+    end
   end
-end
 end
