03333550019d85aeab546057c1fc83edcc6cb6ba
MiaKoda Grodsky <mcgrodsky@gmail.com>
2015-11-20 09:43:05 -0500

    changes before screencast recording

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -3,9 +3,7 @@ class Listing < ActiveRecord::Base
   belongs_to :user
 
     def self.search(search)
-      puts "*" * 50
       if search
-        puts "a" * 50
         where("lower(name) LIKE ?", "%#{search.downcase}%")
       else
         all
